e2b0fa32759351e3046b4d0cb9c00dd2f8fce517
andyh <hurby@ssfs.org>
2020-04-23 09:25:44 -0400

    done with the program

---
+++ src/TokenPass.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+
+public class TokenPass {
+
+    private int[] board;
+    private int currentPlayer;
+
+    /** Creates the board array to be of size playerCount and fills it with
+     * random integer values from 1 to 10, inclusive. Initializes currentPlayer to a
+     * random integer value in the range between 0 and playerCount-1, inclusive.
+     * @param playerCount the number of players
+     */
+    public TokenPass(int playerCount) {
+        board = new int[playerCount];
+        Random num = new Random();
+        for (int a=0; a<playerCount; a++) {
+            int ran_1 = num.nextInt(10);
+            if (ran_1 != 0) {
+                board[a] = ran_1;
+            } else {
+                board[a] = 1;
+            }
+        }
+        currentPlayer = num.nextInt(playerCount);
+
+    }
+
+    /** Distributes the tokens from the current player's position one at a time to each player in
+     * the game. Distribution begins with the next position and continues until all the tokens
+     * have been distributed. If there are still tokens to distribute when the player at the
+     * highest position is reached, the next token will be distributed to the player at position 0.
+     * Precondition: the current player has at least one token.
+     * Postcondition: the current player has not changed.
+     */
+    public void distributeCurrentPlayerTokens() {
+
+
+
+        int Token = board[currentPlayer];
+        int b = currentPlayer;
+        board[b] = 0;
+        b++;
+        while (Token != 0) {
+            board[b]++;
+            Token--;
+            b++;
+            if(b == board.length) {
+                b=0;
+            }
+        }
+
+    }
+    public int getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public int[] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[] board) {
+        this.board = board;
+    }
+
+    public void setCurrentPlayer(int currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+}
